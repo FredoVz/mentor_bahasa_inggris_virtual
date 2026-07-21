7a816103268b16d0ac794ada45c9bdbce15e7c82
FredoVz <dm.fredo018@gmail.com>
2026-07-21 23:16:32 +0700

    feat: core: llm

--- src/core/llm.py
+++ src/core/llm.py
@@ -1,1 +1,16 @@
 # Sifatnya menyimpan instans dari klien sdk Gemini
+
+import src.core.env as env
+
+from functools import lru_cache
+from google import genai
+
+@lru_cache(maxsize=1)
+def get_gemini_client():
+    gemini_client = genai.Client(api_key=env.GEMINI_API_KEY)
+
+    return gemini_client
+
+# Kenapa pembuatan cient dipusatkan pada llm.py?
+# Karena membuat klien ada ongkosnya menyiapkan koneksi, membaca konfigurasi env, dan seterusnya
+# Kalau setiap kali kita butuh gemini klien kita bikin baru maka itu pemborosan
