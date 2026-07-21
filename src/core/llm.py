5b56b371eb857caac061bac3b41230ef5a3cab85
FredoVz <dm.fredo018@gmail.com>
2026-07-21 23:21:37 +0700

    feat: add file pertanyaan_jawaban.txt

--- src/core/llm.py
+++ src/core/llm.py
@@ -10,7 +10,3 @@ def get_gemini_client():
     gemini_client = genai.Client(api_key=env.GEMINI_API_KEY)
 
     return gemini_client
-
-# Kenapa pembuatan cient dipusatkan pada llm.py?
-# Karena membuat klien ada ongkosnya menyiapkan koneksi, membaca konfigurasi env, dan seterusnya
-# Kalau setiap kali kita butuh gemini klien kita bikin baru maka itu pemborosan
