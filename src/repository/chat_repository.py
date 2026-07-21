bfac68b1518dce69af4cce0eda546d6fd6ecd234
FredoVz <dm.fredo018@gmail.com>
2026-07-21 22:35:48 +0700

    feat: repository: chat repository

--- src/repository/chat_repository.py
+++ src/repository/chat_repository.py
@@ -1,4 +1,76 @@
  # satu-satunya lapisan data untuk masuk ke database. Semua layer dari aplikasi kita apabila membutuhkan akses ke database terutama ke tabel chat history. Mengakses melalui file chat repository
 
+import src.core.supabase as supabase
+
+from typing import Literal
+from loguru import logger
+
 class ChatRepository:
-    pass
+    def __init__(self):
+        self.supabase = supabase.get_supabase_client()
+
+    def save_message(
+        self, user_id: int, role: Literal["user", "model"], message_text: str
+    ): # role -> user atau model
+        # chaining function
+        self.supabase.table("chat_histories").insert(
+            {"user_id": user_id, "role": role, "message_text": message_text}
+        ).execute()
+
+    def load_history_by_user_id(self, user_id: int):
+        result = (
+            self.supabase.table("chat_histories")
+            .select("role", "message_text")
+            .eq("user_id", user_id)
+            .order("created_at", desc=False)
+            .execute()
+        )
+
+        return result
+    
+    def save_user(self, user_id: int, username: str, chat_id: int):
+        exist_user = (
+            self.supabase.table("chat_users")
+            .select("user_id")
+            .eq("user_id", user_id)
+            .execute()
+        )
+
+        if len(exist_user.data) > 0:
+            logger.debug(f"user {user_id} sudah terdaftar")
+            return # berhenti
+        
+        result = self.supabase.table("chat_users").insert(
+            {"user_id": user_id, "username": username, "chat_id": chat_id}
+        )
+
+        return result
+    
+    def save_artifact(self, user_id: int, artifact_path):
+        self.supabase.table("chat_histories").insert(
+            {
+                "user_id": user_id, 
+                "role": "model", 
+                "message_text": "[audio]", 
+                "artifact": str(artifact_path),
+            }
+        ).execute()
+
+    def get_last_artifact_by_user_id(self, user_id: int):
+        result = (
+            self.supabase.table("chat_histories")
+            .select("artifact")
+            .eq("user_id", user_id)
+            .not_.is_("artifact", "null") # artifact yang tidak kosong (null)
+            .order("created_at", desc=True)
+            .limit(1)
+            .execute()
+        )
+
+        return result
+
+# chat_repository = ChatRepository() # instansiasi
+
+# 1. Kenapa kita menyimpan riwayat percakapan di database, bukan di Python saja?
+# 2. Dari dua API key yang diberikan supabase, mana yang kita pakai sebagai SUPABASE_KEY di aplikasi?
+# 3. Apa fungsi dekorator @lru_cache pada function `get_supabase_client()`?
