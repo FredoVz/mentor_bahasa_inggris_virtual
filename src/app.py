ab6a2521726db8d0b1449866e76d0819c361f29b
FredoVz <dm.fredo018@gmail.com>
2026-07-21 19:50:56 +0700

    setup supabase

--- src/app.py
+++ src/app.py
@@ -1,1 +1,20 @@
 # Entry point untuk bot telegram
+
+import os
+import supabase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+supabase_client = supabase.create_client(
+    supabase_url=os.getenv("SUPABASE_URL"), supabase_key=os.getenv("SUPABASE_KEY")
+)
+
+# TEST: ambil data dari table chat_users
+result = supabase_client.table("chat_users").select("*").execute()
+print("Koneksi berhasil!")
+print(f"Jumlah user terdaftar: {len(result.data)}")
+
+# Testing
+# cd src
+# uv run app.py
--- src/core/env.py
+++ src/core/env.py
@@ -1,1 +1,18 @@
 # untuk me load atau untuk mengeluarkan dan melakukan validasi env
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+
+def _required_env(name: str) -> str:
+    """Ambil env wajib. Apabila gagal, tampilkan pesan error"""
+
+    value = os.getenv(name)
+
+    if not value:
+        raise RuntimeError()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
