fa565c8e9cacf33b0d27fe93ac3a6647fc2a965d
FredoVz <dm.fredo018@gmail.com>
2026-07-29 10:24:55 +0700

    update codes in schemas.py file

--- src/core/schemas.py
+++ src/core/schemas.py
@@ -8,49 +8,49 @@ from pydantic import BaseModel, Field
 from typing import Literal
 
 class ListeningExerciseSchema(BaseModel):
-    speaker_one: str = Field(... , description="Nama pembicara pertama, misalnya: 'Joe'")
-    speaker_two: str = Field(... , description="Nama pembicara kedua, misalnya: 'Jane'")
+    speaker_one: str = Field(..., description="Nama pembicara pertama, misalnya: 'Joe'")
+    speaker_two: str = Field(..., description="Nama pembicara kedua, misalnya: 'Jane'")
     script: str = Field(
-        ... , 
-        description="Dialog yang dibacakan oleh TTS (Text-to-Speech), format: 'Joe: ...\\nJane: ...' bergantian",
+        ..., 
+        description="Dialog yang dibacakan oleh TTS (Text-to-Speech), format: 'Joe: ...\\nJane:...' bergantian",
     )
     questions: list[str] = Field(
-        ... , 
+        ..., 
         description="Daftar pertanyaan untuk menguji pemahaman peserta berdasarkan `script`",
     )
 
 class EvaluateUserIntentionSchema(BaseModel):
     skill_types: Literal["reading", "speaking", "writing", "listening"] = Field(
-        ... , description="Pilihan salah satu skill_types yang dibutuhkan peserta"
+        ..., description="Pilihan salah satu skill_types yang dibutuhkan peserta"
     )
 
 class LearningSkillTypesSchema(BaseModel): # item latihan yang dilakukan peserta
     category: str = Field(
-        ... , 
+        ..., 
         description="Salah satu kategori skill_types: reading, speaking, writing, dan listening",
     )
-    title: str = Field(... , description="Judul latihan")
+    title: str = Field(..., description="Judul latihan")
     feedback: str = Field(
-        ... , description="Penilaian objective dengan metode sandwich feedback"
+        ..., description="Penilaian objective dengan metode sandwich feedback"
     )
-    score: int = Field(... , description="Nilai kemampuan dalam rentang 1 - 10")
+    score: int = Field(..., description="Nilai kemampuan dalam rentang 1 - 10")
 
 class LearningReportSchema(BaseModel): # laporan belajar
-    start_date: str = Field(... , description="Tanggal mulai belajar")
-    end_date: str = Field(... , description="Tanggal akhir belajar")
-    username: str = Field(... , description="Username dari peserta")
-    global_score: int = Field(... , description="Nilai keseluruhan")
+    start_date: str = Field(..., description="Tanggal mulai belajar")
+    end_date: str = Field(..., description="Tanggal akhir belajar")
+    username: str = Field(..., description="Username dari peserta")
+    global_score: int = Field(..., description="Nilai keseluruhan")
     skill_types: list[LearningSkillTypesSchema] # list of LearningSkillTypesSchema
     markdown_content: str = Field(
-        ... , description="Seluruh isi laporan dalam format markdown"
+        ..., description="Seluruh isi laporan dalam format markdown"
     )
 
 class EvaluateSpeakingSchema(BaseModel):
     correction: str = Field(
-        ... , description="Catatan perbaikan pengucapan bahasa inggris untuk peserta"
+        ..., description="Catatan perbaikan pengucapan bahasa inggris untuk peserta"
     )
     score: str = Field(
-        ... , 
+        ..., 
         description="Rentang nilai dari 1 - 10 untuk pengucapan bahasa inggris dari peserta",
     )
-    summary: str = Field(... , description="Gabungan antara correction dan score")
+    summary: str = Field(..., description="Gabungan antara correction dan score")
