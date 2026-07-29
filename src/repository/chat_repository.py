25f4b476e695f8a405fcfd502a010f1b9620e066
FredoVz <dm.fredo018@gmail.com>
2026-07-29 10:38:22 +0700

    update codes in chat_repository.py file

--- src/repository/chat_repository.py
+++ src/repository/chat_repository.py
@@ -40,8 +40,10 @@ class ChatRepository:
             logger.debug(f"user {user_id} sudah terdaftar")
             return # berhenti
         
-        result = self.supabase.table("chat_users").insert(
-            {"user_id": user_id, "username": username, "chat_id": chat_id}
+        result = (
+            self.supabase.table("chat_users")
+            .insert({"user_id": user_id, "username": username, "chat_id": chat_id})
+            .execute()
         )
 
         return result
@@ -69,4 +71,5 @@ class ChatRepository:
 
         return result
 
-# chat_repository = ChatRepository() # instansiasi
+    def get_users(self):
+        return self.supabase.table("chat_users").select("user_id").execute()
