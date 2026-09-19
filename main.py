2476ad0eda001e4d75358536cdf74dffa9cc8ccb
FredoVz <dm.fredo018@gmail.com>
2026-09-20 00:52:53 +0700

    Add file instruction terminal webhook and update file main.py

--- main.py
+++ main.py
@@ -22,23 +22,3 @@ async def telegram_webhook(request: Request):
     req_body = await request.json()
     logger.debug(req_body)
     return Response(status_code=status.HTTP_200_OK)
-
-# terminal cara jalankan API
-# uv run fastapi dev
-
-# http://127.0.0.1:8000/
-# localhost:8000
-
-# terminal untuk coba post
-# curl -X POST http://127.0.0.1:8000/post / localhost:8000/post
-# curl.exe -X POST localhost:8000/post
-
-# Invoke-WebRequest -Method POST -Uri http://127.0.0.1:8000/post
-
-# webhook
-# curl.exe -X POST localhost:8000/webhook \
-# -H "Content-Type: application/json" \
-# -d '{"data": "ini contoh data dari server telegram"}'
-
-# webhook for powershell windows
-# curl.exe -X POST http://localhost:8000/webhook -H "Content-Type: application/json" -d "{\`"data\`": \`"ini contoh data dari server telegram\`"}"
